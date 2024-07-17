a42246b54f55d3903f48f2cff1f8297407226ab6
JeonB <newjeonb@gmail.com>
2024-07-17 18:39:04 +0900

    페이지간 연결하기

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,8 +1,17 @@
+import { Fragment } from "react";
+import Link from "next/link";
 export default function NewsPage() {
   return (
-    <div>
+    <Fragment>
       <h1>News Page</h1>
       <p>This is the News page</p>
-    </div>
+      <ul>
+        <li>
+          <Link href="/news/news1">News 1</Link>
+        </li>
+        <li>News 2</li>
+        <li>News 3</li>
+      </ul>
+    </Fragment>
   );
 }
